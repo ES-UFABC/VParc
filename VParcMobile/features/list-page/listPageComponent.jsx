158544f7ee631799648e91bece658e0d3a317f91
bianca.kanehira <bdemi99@gmail.com>
2022-04-24 02:56:44 -0300

    add load more advertisement

--- VParcMobile/features/list-page/listPageComponent.jsx
+++ VParcMobile/features/list-page/listPageComponent.jsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { View, Text,ScrollView } from "react-native";
+import { View, Text,ScrollView, TouchableOpacity } from "react-native";
 import { Appbar,  Searchbar, Drawer, Card, Title, Paragraph, Button, FAB, Portal, Menu } from "react-native-paper";
 import colors from "../../styles/colors";
 import styles from "../../styles/styleListPage";
+import { useFonts } from "@expo-google-fonts/nunito";
+import { 
+    Nunito_200ExtraLight,
+    Nunito_200ExtraLight_Italic,
+    Nunito_300Light,
+    Nunito_400Regular,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  } from '@expo-google-fonts/nunito';
 import { useAuth } from "../../context/userAuth";
+import AppLoading from 'expo-app-loading';
 import { getAll } from "../../services/advertisementService";
 
 const AnuncioCardComponent = (props) =>{
@@ -32,41 +42,81 @@ const ListPageComponent = ({navigation}) => {
     const [advertisementList, setAdList] = useState([]);
     const [loaded, setLoaded] = useState(false);
     const [optionsVisible, setOptions] = useState(false);
+    const [showMoreButton, setShowMoreButton] = useState(false);
+    const [itensShowed, setItensShowed] = useState(10);
+    const [searchBarValue, setSearchBarValue] = useState('');
     const handleLogout = async ()=> { await logout();}
     const viewProfile = () => {navigation.push('UserProfile')}
     const loadAds = async() =>{
         await getAll().then(
             (ads)=>{
-                setAdList(ads);
-                setOriginalAdList(ads);
+                setOriginalAdList(ads.reverse());
+                filterAdsQuantity(ads.length);
                 setLoaded(true);
             }
         )
+    
     }
+
     const openOption = () =>{
         setOptions(true);
     }
     const closeOption = () =>{
         setOptions(false);
     }
+
+    const filterAdsQuantity = (adsQuantity) => {
+        adsQuantity > itensShowed ? setShowMoreButton(true) : setShowMoreButton(false);
+        if (showMoreButton) {
+            let newAdList = [];
+            originalAdList.slice(0, itensShowed).map(
+                (item) => {
+                    newAdList.push(item);
+                }
+            )
+            setAdList(newAdList);
+            setItensShowed(itensShowed + 10);
+        } else {
+            setAdList(originalAdList);
+        }
+    }
+
     if(!loaded){
         loadAds();
     }
     const filterAdvertisement = (text) =>{
+        setSearchBarValue(text);
         let newAdList = [];
-        originalAdList.map((ad)=>{
-            if(ad.title.toLowerCase().includes(text.toLowerCase())){
-                newAdList.push(ad);
-            }   
-        })
-        setAdList(newAdList);
+        originalAdList.slice(0, itensShowed - 10).map(
+            (ad)=>{
+                if(ad.title.toLowerCase().includes(text.toLowerCase())){
+                    newAdList.push(ad);
+                }   
+            }
+        )
+        setAdList(newAdList);   
     }
+
+    const handleLoadMore = () => {
+        setSearchBarValue("");
+        filterAdsQuantity(originalAdList.length);
+    }
+
+    let [fontsLoaded] = useFonts({
+        Nunito_400Regular,
+        Nunito_700Bold,
+        Nunito_800ExtraBold
+      });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
     return(
         <View>
             <Appbar.Header style={styles.appBar}>
-                <Searchbar placeholder="Pesquisar" onChangeText={(text)=>filterAdvertisement(text)} style={styles.searchBar}/>
+                <Searchbar placeholder="Pesquisar" value={searchBarValue} onChangeText={(text)=>filterAdvertisement(text)} style={styles.searchBar}/>
                 <Appbar.Action icon='refresh' onPress={() => setLoaded(false)}/>
-                {/* <Appbar.Action icon='account' onPress={() => handleLogout()}/> */}
                 <Menu visible={optionsVisible} onDismiss={closeOption} anchor={<Appbar.Action icon='account'  onPress={()=>openOption()}/>}>
                     <Menu.Item onPress={()=>{handleLogout(); closeOption()}} title="Deslogar"/>
                     <Menu.Item onPress={()=>{viewProfile(); closeOption()}} title="Ver Perfil"/>
@@ -74,13 +124,32 @@ const ListPageComponent = ({navigation}) => {
                 </Menu>
             </Appbar.Header>
             <ScrollView>
-            {
-                advertisementList.map((advertisement, index)=>{
-                    return(
-                        <AnuncioCardComponent key={index} advertisement={advertisement} navigation={navigation}/>
-                    )
-                })
-            }
+                {
+                    advertisementList.map((advertisement, index)=>{
+                        return(
+                            <AnuncioCardComponent key={index} advertisement={advertisement} navigation={navigation}/>
+                        )
+                    })
+                }
+                {showMoreButton ? 
+                    <TouchableOpacity 
+                        style={{ 
+                            width: "40%",
+                            height: 50,
+                            alignItems: "center",
+                            marginTop: 20,
+                            marginLeft: '2%',
+                            justifyContent:'center', 
+                            backgroundColor:colors.primary
+                        }} 
+                        onPress={() => handleLoadMore()}
+                    >
+                        <Text style={styles.textButton}>Carregar mais</Text>
+                    </TouchableOpacity>
+                    :
+                    null    
+                }
+                <View style = { styles.spacerStyle }></View>
             </ScrollView>
             <View >
                     <FAB
--- VParcMobile/styles/styleListPage.jsx
+++ VParcMobile/styles/styleListPage.jsx
@@ -10,6 +10,14 @@ const styles = StyleSheet.create({
     searchBar:{
         backgroundColor:colors.secundary,
         width:'65%'
+    },
+    textButton:{
+        color:colors.white,
+        fontFamily:'Nunito_700Bold',
+        fontSize:18
+    },
+    spacerStyle: {
+        marginBottom: 15
     }
 })
 
