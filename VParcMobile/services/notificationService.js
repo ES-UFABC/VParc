be840bbac27b7d011c1ac8c63fed8e19374def7f
GuOlival <goaguerra@gmail.com>
2022-05-08 23:36:47 -0300

    Inclusão de variáveis de ambiente

--- VParcMobile/services/notificationService.js
+++ VParcMobile/services/notificationService.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:3000/api';
-
 const notifyInterest = async(notification)=>{
     let response ={};
-    await axios.post(BASE_URL + '/user/createNotification', notification)
+    await axios.post(process.env.BASE_URL + '/user/createNotification', notification)
         .then(res =>{
             response = res;
         })
@@ -16,9 +14,8 @@ const notifyInterest = async(notification)=>{
 
 const getNotifications = async(user)=>{
     let response = {};
-    await axios.post(BASE_URL + '/user/getNotifications', user)
+    await axios.post(process.env.BASE_URL + '/user/getNotifications', user)
         .then((res)=>{
-            console.log(res);
             response = res.data.data.data.result;
         })
         .catch(err=>{
@@ -30,8 +27,7 @@ const getNotifications = async(user)=>{
 
 const readNotification = async(notification)=>{
     let response = {};
-    console.log(notification);
-    await axios.post(BASE_URL + '/user/readNotification', notification)
+    await axios.post(process.env.BASE_URL + '/user/readNotification', notification)
         .then((res)=>{
             response = res;
         })
